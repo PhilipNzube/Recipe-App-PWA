1b33565c1cd3145684aef98aa7e2341028a10d61
PhilipNzube <72675495+PhilipNzube@users.noreply.github.com>
2023-01-12 04:44:10 +0100

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -5,7 +5,7 @@ this.addEventListener("install", (event) => {
             cache.addAll([
                 '/static/js/bundle.js',
                 '/static/js/main.2ca0110c.js',
-                '/static/css/main.50802479.css',
+                '/static/css//main.50802479.css',
                 '/manifest.json',
                 '/static/media/BG.4b166807ad62b966f477.jpg',
                 '/static/media/vegetable.5c2ee5bb1195793cc921.jpg',
@@ -14,6 +14,8 @@ this.addEventListener("install", (event) => {
                 '/static/media/chin.f089efc237a75101cc37.jpeg',
                 '/static/media/fried.81a9006b4052bced3207.jpeg',
                 '/static/media/ewedu1.995d3a754eec1a8d7b1f.jpeg',
+                '/static/media/fishpepper-soup.a6675fa5a07bf8e1da69.jpg',
+                '/static/media/ice%20cream.c2b4233f97efd1573030.jfif',
                 '/ICON.png',
                 '/ICON192.png',
                 '/ICON512.png',
